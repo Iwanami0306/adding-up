03a2adc689a0ad71a22a727934fd5c34b4fbb80e
Iwanami <turmeric.naminami.1073@gmail.com>
2020-01-07 10:52:33 +0000

    減少ランキングへの変更

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ rl.on('close',() => { //読み終わり時に実行
         value.change = value.popu15 / value.popu10;
     }
     const rankingArray = Array.from(map).sort((pair1,pair2) => {
-        return pair2[1].change -pair1[1].change;//大きい順
+        return pair1[1].change -pair2[1].change;//-ならpair1が前,+ならpair2が前
     });
-    const rankingStrings =rankingArray.map((pair) => { //map関数:array要素の内容を変換
-        return pair[0] + ':' + pair[1].popu10 +'=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+    const rankingStrings =rankingArray.map((pair,i) => { //map関数:array要素の内容を変換
+        return (i+1) + '位' + pair[0] + ':' + pair[1].popu10 +'=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
     console.log(rankingStrings);
 })
